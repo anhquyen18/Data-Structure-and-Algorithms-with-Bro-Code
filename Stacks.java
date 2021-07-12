b4cce9a4eff1fd4a04a6da80eae154f5e6b7428d
anhquyen18 <anhquyen18092000@gmail.com>
2021-07-12 21:23:51 +0700

    test commit

---
+++ Stacks.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Stacks {
+
+	public static void main(String[] args) {
+		
+		Stack<String> stack =  new Stack<String>();	
+		
+		stack.push("Minecraft");
+		stack.push("Skyrim");
+		stack.push("DOOM");
+		stack.push("Borderlands");
+		stack.push("FFVII");
+		
+//		String myFavGame = stack.pop();
+//		System.out.println(stack.peek());
+//		System.out.println(stack);
+		
+		//uses of stacks?
+		// 1. undo/redo feature in text editors
+		// 2. moving back/ forward through brower history
+		// 3. backtracking algorithms (maze, file directories)
+		// 4. calling functions (call stack)
+	}
+
+}
