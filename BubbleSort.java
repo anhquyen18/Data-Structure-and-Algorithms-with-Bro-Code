22fe1097e57a087c9477b8600a570f0a6bc0f9c3
anhquyen18 <anhquyen18092000@gmail.com>
2021-07-13 21:47:09 +0700

    learn insertion sort

--- BubbleSort.java
+++ BubbleSort.java
@@ -9,13 +9,13 @@ public class BubbleSort {
 	
 	public static void main(String[] args) {
 		int array[] = { 9, 1, 8, 2, 7, 3, 6, 4, 5 };
-		bubleSort(array);
+		bubbleSort(array);
 		for (int i : array) {
 			System.out.print(i);
 		}
 	}
 
-	private static void bubleSort(int array[]) {
+	private static void bubbleSort(int array[]) {
 		for (int i = 0; i < array.length - 1; i++) {
 			for (int j = 0; j < array.length - i - 1; j++) {
 				if (array[j] > array[j+1]) {
